832d81f1afb62a18f44b67bb2ae665c3fd8f7344
endrycofr <endrycofarelr@gmail.com>
2024-02-21 20:37:27 +0700

    Email Checking Endpoint

--- handler/user.go
+++ handler/user.go
@@ -57,7 +57,7 @@ func (h *userHandler) Login(c *gin.Context) {
 // mencocokan password 
 
 var input user.LoginInput
-err := c.ShouldBind(&input)
+err := c.ShouldBindJSON(&input)
 if err != nil {
 	errors := helper.FormatValidationError(err)
 	errorMassage := gin.H{"error": errors}
@@ -76,4 +76,39 @@ if err != nil {
 	response := helper.ApiResponse("Successfully loggedin", http.StatusOK, "success", formatter)
 
 	c.JSON(http.StatusOK, response)
+}
+
+
+func (h *userHandler) CheckEmailAvailability(c *gin.Context) {
+    var input user.CheckEmailInput
+
+
+	err := c.ShouldBindJSON(&input)
+	if err != nil{
+			errors := helper.FormatValidationError(err)
+			errorMassage := gin.H{"error": errors}
+			response := helper.ApiResponse("Email Checking Failed", http.StatusUnprocessableEntity, "error", errorMassage)
+			c.JSON(http.StatusUnprocessableEntity, response)
+			return
+	}
+
+    isEmailAvailable, err := h.userService.IsEmailAvailable(input)
+    if err != nil {
+        errorMassage := gin.H{"error": "Server Error"}
+        response := helper.ApiResponse("Email Checking Failed", http.StatusUnprocessableEntity, "error", errorMassage)
+        c.JSON(http.StatusUnprocessableEntity, response)
+        return
+    }
+
+    data := gin.H{
+        "is_available": isEmailAvailable,
+    }
+
+    metaMessage := "Email has been registered"
+    if isEmailAvailable {
+        metaMessage = "Email is available"
+    }
+
+    response := helper.ApiResponse(metaMessage, http.StatusOK, "success", data)
+    c.JSON(http.StatusOK, response)
 }
--- user/service.go
+++ user/service.go
@@ -9,6 +9,7 @@ import (
 type Service interface {
 	RegisterUser(input RegisterUserInput) (User, error)
 	Login(input LoginInput) (User, error)
+	IsEmailAvailable(input CheckEmailInput) (bool, error)
 }
 
 type service struct {
@@ -60,3 +61,15 @@ func (s *service)Login(input LoginInput) (User, error) {
 	return user, nil
 }
 // mapping struct input ke struct User
+
+func (s *service) IsEmailAvailable (input CheckEmailInput) (bool, error){
+	email := input.Email
+	user, err := s.repository.FindByEmail(email)
+	if err != nil {
+		return false, err
+	}
+	if user.ID == 0 {
+		return true, nil
+	}
+	return false, nil
+}
