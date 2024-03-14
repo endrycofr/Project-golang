52ff925286e5e298652da4a3b39603b24f0eba49
endrycofr <endrycofarelr@gmail.com>
2024-03-14 14:47:34 +0700

    Init campaign

--- campaign/entity.go
+++ campaign/entity.go
@@ -1,1 +1,27 @@
 package campaign
+
+import "time"
+
+type Campaign struct {
+	ID               int
+	UserID           int
+	Name             string
+	ShortDescription string
+	Perks            string
+	BackerCount      int
+	GoalAmount       int
+	CurrentAmount    int
+	Slug             string // Huruf besar pada "Slug"
+	CreatedAt        time.Time
+	UpdatedAt        time.Time
+	CampaignImages   []CampaignImage // Menggunakan foreignKey untuk menunjukkan kunci asing
+}
+
+type CampaignImage struct {
+	ID         int
+	CampaignID int // Huruf besar pada "CampaignID"
+	FileName   string
+	IsPrimary  int
+	CreatedAt  time.Time
+	UpdatedAt  time.Time
+}
