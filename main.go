5cfe0c75202737a2316b0aadce6d19333da0d744
Endryco Farel Rianrachmatullah <88662707+endrycofr@users.noreply.github.com>
2025-02-12 22:17:42 +0700

    memperbaiki code

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ import (
 )
 
 func connectDatabase() (*gorm.DB, error) {
-	dsn := "root:End291103#@tcp(mysql_db:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:End291103#@tcp(db:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
 	var db *gorm.DB
 	var err error
 
