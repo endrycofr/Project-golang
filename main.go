c7053b4358ce6f71371d2ca2300a86aaf5b0e414
Endryco Farel Rianrachmatullah <88662707+endrycofr@users.noreply.github.com>
2025-12-19 21:49:24 +0700

    menambahkan project-images

--- main.go
+++ main.go
@@ -22,6 +22,7 @@ import (
 func main() {
 	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
 	dsn := "root:End291103#@tcp(127.0.0.1:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
+	// dsn := "root@tcp(127.0.0.1:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
